68eecbd0e7a6d7c70850a15c33cc36a288fe6034
Alexander Ryndin <alexander.ryndin@consol.de>
2019-02-26 14:19:46 +0100

    removed debug logging

--- createemployee/src/main/java/de/consol/labs/aws/neptunedemoapp/createemployee/CreateEmployeeHandler.java
+++ createemployee/src/main/java/de/consol/labs/aws/neptunedemoapp/createemployee/CreateEmployeeHandler.java
@@ -7,13 +7,9 @@ import de.consol.labs.aws.neptunedemoapp.common.crud.employee.params.EmployeeVer
 import de.consol.labs.aws.neptunedemoapp.common.facade.GraphFacade;
 import de.consol.labs.aws.neptunedemoapp.common.remote.RemoteClusterAdapter;
 import de.consol.labs.aws.neptunedemoapp.common.remote.RemoteConnectionSettings;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 
 public class CreateEmployeeHandler implements RequestHandler<EmployeeModel, Object> {
-
-    private static final Logger LOG = LogManager.getLogger(CreateEmployeeHandler.class);
 
     @Override
     public Object handleRequest(final EmployeeModel employeeModel, final Context context) {
@@ -26,14 +22,7 @@ public class CreateEmployeeHandler implements RequestHandler<EmployeeModel, Obje
             try (final GraphTraversalSource g = adapter.getGraphTraversalSource()) {
                 final GraphFacade gf = new GraphFacade();
                 final EmployeeVertex v = gf.getEmployeeCrudController().create(g, employeeModel);
-                final Object id = v.id();
-                LOG.info(
-                        "vertex ID = {}, first name = {}, last name = {}",
-                        id,
-                        v.property("firstName").value(),
-                        v.property("lastName").value()
-                );
-                return id;
+                return v.id();
             } catch (final Exception e) {
                 throw new RuntimeException(e);
             }
