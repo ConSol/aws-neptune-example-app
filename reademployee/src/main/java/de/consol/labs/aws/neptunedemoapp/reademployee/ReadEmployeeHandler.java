b70daba6ed69cf72d76e7d9f4d04a6a7a5d38ee2
Alexander Ryndin <alexander.ryndin@consol.de>
2019-02-26 14:20:43 +0100

    fixed the "The property does not exist" bug in an ad hoc manner

--- reademployee/src/main/java/de/consol/labs/aws/neptunedemoapp/reademployee/ReadEmployeeHandler.java
+++ reademployee/src/main/java/de/consol/labs/aws/neptunedemoapp/reademployee/ReadEmployeeHandler.java
@@ -2,23 +2,24 @@ package de.consol.labs.aws.neptunedemoapp.reademployee;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import de.consol.labs.aws.neptunedemoapp.common.crud.Label;
 import de.consol.labs.aws.neptunedemoapp.common.crud.employee.params.EmployeeId;
-import de.consol.labs.aws.neptunedemoapp.common.crud.employee.params.EmployeeModel;
-import de.consol.labs.aws.neptunedemoapp.common.facade.GraphFacade;
 import de.consol.labs.aws.neptunedemoapp.common.remote.RemoteClusterAdapter;
 import de.consol.labs.aws.neptunedemoapp.common.remote.RemoteConnectionSettings;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 
-public class ReadEmployeeHandler implements RequestHandler<EmployeeId, EmployeeModel> {
+public class ReadEmployeeHandler implements RequestHandler<EmployeeId, Map<Object, Object>> {
 
     private static final Logger LOG = LogManager.getLogger(ReadEmployeeHandler.class);
 
     @Override
-    public EmployeeModel handleRequest(final EmployeeId employeeId, final Context context) {
+    public Map<Object, Object> handleRequest(final EmployeeId employeeId, final Context context) {
         final RemoteConnectionSettings settings = new RemoteConnectionSettings()
                 .setEndpoint(System.getenv(EnvironmentVariable.NEPTUNE_ENDPOINT))
                 .setPort(Integer.parseInt(System.getenv(EnvironmentVariable.NEPTUNE_PORT)))
@@ -26,15 +27,16 @@ public class ReadEmployeeHandler implements RequestHandler<EmployeeId, EmployeeM
                 .setKeyCertChainFile(getClass().getClassLoader().getResource("SFSRootCAG2.pem").getFile());
         try (final RemoteClusterAdapter adapter = new RemoteClusterAdapter(settings)) {
             try (final GraphTraversalSource g = adapter.getGraphTraversalSource()) {
-                final GraphFacade gf = new GraphFacade();
-                final Optional<EmployeeModel> employee = gf.getEmployeeCrudController().read(g, employeeId, EmployeeModel.class);
-                if (!employee.isPresent()) {
+                final Optional<Map<Object, Object>> employeeProperties = g.V()
+                        .hasLabel(Label.Vertex.EMPLOYEE)
+                        .has("id", employeeId.getId())
+                        .valueMap()
+                        .tryNext();
+                if (!employeeProperties.isPresent()) {
                     LOG.info("could not find employee with ID {}", employeeId.getId());
-                    return null;
+                    return Collections.emptyMap();
                 }
-                final EmployeeModel result = employee.get();
-                LOG.info("employee: {}", result);
-                return result;
+                return employeeProperties.get();
             } catch (final Exception e) {
                 throw new RuntimeException(e);
             }
